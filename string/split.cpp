b6cd7a4575abc65581f4325a6ad98fdffd230f3f
ShinyaKato <k.shin220@gmail.com>
2017-12-16 23:29:36 +0900

    Modify string split

--- string/split.cpp
+++ string/split.cpp
@@ -6,12 +6,14 @@ typedef long long ll;
 vector<string> split(string s, string p) {
   vector<string> ret;
   ll h = 0;
+
   REP(i, 0, s.size() - p.size() + 1) if(s.substr(i, p.size()) == p) {
     ret.push_back(s.substr(h, i - h));
     h = i + p.size();
     i += (ll) p.size() - 1;
   }
   ret.push_back(s.substr(h, (ll) s.size() - h));
+
   return ret;
 }
 
@@ -22,6 +24,4 @@ int main(void) {
   assert(sp.size() == 10);
   string expect[] = { "apple", "banana", "orange", "potato", "", "tomato", "", "", "fish", "" };
   REP(i, 0, sp.size()) assert(sp[i] == expect[i]);
-
-  return 0;
 }
