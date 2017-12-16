5aa19bcb1c4419b328cfe6e6e7002d2173530cf4
ShinyaKato <k.shin220@gmail.com>
2017-12-16 23:27:16 +0900

    Modify string split

--- string/split.cpp
+++ string/split.cpp
@@ -1,24 +1,27 @@
-#include <iostream>
-#include <vector>
-#define REP(i, a, n) for(int i = ((int) a); i < ((int) n); i++)
+#include <bits/stdc++.h>
+#define REP(i, a, n) for(ll i = ((ll) a); i < ((ll) n); i++)
 using namespace std;
+typedef long long ll;
 
 vector<string> split(string s, string p) {
   vector<string> ret;
-  int h = 0;
+  ll h = 0;
   REP(i, 0, s.size() - p.size() + 1) if(s.substr(i, p.size()) == p) {
     ret.push_back(s.substr(h, i - h));
     h = i + p.size();
-    i += (signed) p.size() - 1;
+    i += (ll) p.size() - 1;
   }
-  ret.push_back(s.substr(h, (signed) s.size() - h));
+  ret.push_back(s.substr(h, (ll) s.size() - h));
   return ret;
 }
 
 int main(void) {
-  string s = "apple<!>banana<!>orange<!>potato<!><!>tomato<!><!><!>fish";
+  string s = "apple<!>banana<!>orange<!>potato<!><!>tomato<!><!><!>fish<!>";
   vector<string> sp = split(s, "<!>");
-  REP(i, 0, sp.size()) cout << sp[i] << endl;
+
+  assert(sp.size() == 10);
+  string expect[] = { "apple", "banana", "orange", "potato", "", "tomato", "", "", "fish", "" };
+  REP(i, 0, sp.size()) assert(sp[i] == expect[i]);
 
   return 0;
 }
