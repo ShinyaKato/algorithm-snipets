15213e47744726db60e8fb92b467bdf16b773a9e
ShinyaKato <k.shin220@gmail.com>
2018-01-24 22:39:14 +0900

    modify monotone minima

--- dynamic_programming/monotone_minima.cpp
+++ dynamic_programming/monotone_minima.cpp
@@ -4,17 +4,17 @@
 using namespace std;
 typedef long long ll;
 
-/* il, ir, jl, jr: [il, ir), [jl, jr]
+/* il, ir, jl, jr: [il, ir), [jl, jr)
  * f(i, j):
  *   arg(i) := f(i, j)のあるiを決めた時にf(i, _)に最小を与えるj
  *   j1 = arg(i1), j2 = arg(i2), i1 < i2ならばj1 <= j2
  */
-void dfs(ll il, ll ir, ll jl, ll jr, vector<ll> &ans, function<ll(ll, ll)> &f) {
+void dfs(ll il, ll ir, ll jl, ll jr, function<ll(ll, ll)> &f, vector<ll> &ans) {
   if(il >= ir) return;
 
   ll im = (il + ir) / 2, jm;
   ans[im] = INF;
-  REP(j, jl, jr + 1) {
+  REP(j, jl, jr) {
     ll v = f(im, j);
     if(ans[im] > v) {
       ans[im] = v;
@@ -22,8 +22,8 @@ void dfs(ll il, ll ir, ll jl, ll jr, vector<ll> &ans, function<ll(ll, ll)> &f) {
     }
   }
 
-  dfs(il, im, jl, jm, ans, f);
-  dfs(im + 1, ir, jm, jr, ans, f);
+  dfs(il, im, jl, jm + 1, f, ans);
+  dfs(im + 1, ir, jm, jr, f, ans);
 }
 
 int main() {
@@ -32,7 +32,7 @@ int main() {
   ll V1[N1] = { 1, 2, 2 };
   vector<ll> ans1(N1);
   function<ll(ll, ll)> f1 = [&](ll i, ll j) { return A1[j] + (i - j) * (i - j); };
-  dfs(0, N1, 0, N1 - 1, ans1, f1);
+  dfs(0, N1, 0, N1, f1, ans1);
   REP(i, 0, N1) assert(ans1[i] == V1[i]);
 
   ll N2 = 11;
@@ -40,7 +40,7 @@ int main() {
   ll V2[N2] = { 1, 2, 4, 7, 10, 14, 10, 7, 4, 2, 1 };
   vector<ll> ans2(N2);
   function<ll(ll, ll)> f2 = [&](ll i, ll j) { return A2[j] + (i - j) * (i - j); };
-  dfs(0, N2, 0, N2 - 1, ans2, f2);
+  dfs(0, N2, 0, N2, f2, ans2);
   REP(i, 0, N2) assert(ans2[i] == V2[i]);
 
   ll N3 = 12;
@@ -48,6 +48,6 @@ int main() {
   ll V3[N3] = { 10, 11, 14, 16, 13, 12, 12, 13, 11, 6, 3, 2 };
   vector<ll> ans3(N3);
   function<ll(ll, ll)> f3 = [&](ll i, ll j) { return A3[j] + (i - j) * (i - j); };
-  dfs(0, N3, 0, N3 - 1, ans3, f3);
+  dfs(0, N3, 0, N3, f3, ans3);
   REP(i, 0, N3) assert(ans3[i] == V3[i]);
 }
